ed32c55b76f02fec145f4fafd0cf36de677ea3a4
Reneva Newman <renevanewman7@gmail.com>
2025-11-07 22:22:58 +0500

    added react router dom import in main jsx

--- DJS04-SOLUTION-main/src/main.jsx
+++ DJS04-SOLUTION-main/src/main.jsx
@@ -1,10 +1,27 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom"; // 🧭 React Router imports
+import "./index.css";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
+// (🆕 you'll soon create these two files in src/pages/)
+import HomePage from "./pages/HomePage.jsx";
+import ShowDetailPage from "./pages/ShowDetailPage.jsx";
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    {/* Wrap your entire app in the router */}
+    <BrowserRouter>
+      <Routes>
+        {/* The "App" component will act as your main layout (e.g., nav, header, etc.) */}
+        <Route path="/" element={<App />}>
+          {/* Default route (homepage) */}
+          <Route index element={<HomePage />} />
+          {/* Dynamic route for show details */}
+          <Route path="show/:id" element={<ShowDetailPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
+);
+
